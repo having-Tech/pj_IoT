56d504a8bae435626128c84a95e8a5cb26a83401
having-Tech <43468958+having-Tech@users.noreply.github.com>
2018-12-06 11:16:33 -0500

    versionup

--- iot_openDoor/cctv.py
+++ iot_openDoor/cctv.py
@@ -1,25 +1,33 @@
 import cv2
 import time
+import os
+import datetime
 
 class Camera_cctv():
     camera = cv2.VideoCapture(0)
 
     # Define the codec and create VideoWriter object to save the video
     fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
-    video_writer = cv2.VideoWriter('output.mp4', fourcc, 30.0, (640, 480))
 
-    while True:
-            (grabbed, frame) = camera.read()  # grab the current frame
-            frame = cv2.resize(frame, (640,480))
-            cv2.imshow("Frame", frame)  # show the frame to our screen
-            key = cv2.waitKey(33) & 0xFF
-            video_writer.write(frame)  # Write the video to the file system
+    video_writer = cv2.VideoWriter( 'output.mp4', fourcc, 30.0, (640, 480))
 
-          # I don't really have an idea what this does, but it works..
-    
 
-            if key==27:
-                break
+    #while True:
+    n = 0
+    for x in range(10):
+        (grabbed, frame) = camera.read()  # grab the current frame
+        frame = cv2.resize(frame, (640,480))
+        cv2.imshow("Frame", frame)  # show the frame to our screen
+        key = cv2.waitKey(33) & 0xFF
+        video_writer.write(frame)  # Write the video to the file system
+
+        print(n)
+        time.sleep(1)
+        os.system('clear')
+        n = n + 1
+         # I don't really have an idea what this does, but it works..
+         #if k == 27:
+        #     break
 
     # cleanup the camera and close any open windows
     camera.release()
--- iot_openDoor/face_recognition.py
+++ iot_openDoor/face_recognition.py
@@ -6,9 +6,23 @@ import json
 from collections import OrderedDict
 import datetime
 import sys
+import cctv
+import RPi.GPIO as GPIO
 
 newCode="sudo modprobe bcm2835-v4l2"
 os.system(newCode)
+
+GPIO.setmode(GPIO.BCM)
+
+# 각 숫자는 라즈베리 파이의 숫자를 의미한다.
+#pin 23은 감지 센서, pin20은 성공했을 때의 LED불 pin 21은 실패했을 때의 LED불
+sensor = 23
+Sled = 20
+Fled = 21
+
+GPIO.setup(sensor, GPIO.IN)
+GPIO.setup(Sled, GPIO.OUT)
+GPIO.setup(Fled, GPIO.OUT)
 
 class recognition():
 
@@ -35,90 +49,104 @@ class recognition():
                     with open('result/' + id + '.json', 'w') as make_file:
                         json.dump(file_data, make_file)
 
-    # 얼굴 트레이너 불러오기
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
-    recognizer.read('trainer/trainer.yml')
-    cascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(cascadePath);
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    #iniciate id counter
-    id = 0
-
-    # names related to ids: example ==> Marcelo: id=1,  etc
-    names = ['None', 'minsub', 'jihoon', 'hosic', 'taehoon', 'jieun']
-
-    # Initialize and start realtime video capture
-    cam = cv2.VideoCapture(0)
-    cam.set(3, 640) # set video widht
-    cam.set(4, 480) # set video height
-
-    # Define min window size to be recognized as a face
-    minW = 0.1*cam.get(3)
-    minH = 0.1*cam.get(4)
+    print "Waiting for sensor to settle"
+    time.sleep(2)
+    print "Detecting motion"
 
     while True:
-        # 얼굴 인식전 상태
-        face_recog_state = False
+        if GPIO.input(sensor):
+            print "Motion Detected"
+            # 얼굴 트레이너 불러오기
+            recognizer = cv2.face.LBPHFaceRecognizer_create()
+            recognizer.read('trainer/trainer.yml')
+            cascadePath = "haarcascade_frontalface_default.xml"
+            faceCascade = cv2.CascadeClassifier(cascadePath);
 
-        ret, img =cam.read()
-        img = cv2.flip(img, 1) # Flip vertically
+            font = cv2.FONT_HERSHEY_SIMPLEX
 
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            #iniciate id counter
+            id = 0
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor = 1.2,
-            minNeighbors = 5,
-            minSize = (int(minW), int(minH)),
-           )
+            # names related to ids: example ==> Marcelo: id=1,  etc
+            names = ['None', 'minsub', 'jihoon', 'hosic', 'taehoon', 'jieun']
 
-        for(x,y,w,h) in faces:
+            # Initialize and start realtime video capture
+            cam = cv2.VideoCapture(0)
+            cam.set(3, 640) # set video widht
+            cam.set(4, 480) # set video height
 
-            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            # Define min window size to be recognized as a face
+            minW = 0.1*cam.get(3)
+            minH = 0.1*cam.get(4)
 
-            id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+            while True:
+                # 얼굴 인식전 상태
+                face_recog_state = False
 
-            # Check if confidence is less them 100 ==> "0" is perfect match
-            if (confidence < 100):
-                id = names[id]
-                confidence = "  {0}".format(round(100 - confidence))
+                ret, img =cam.read()
+                img = cv2.flip(img, 1) # Flip vertically
 
-                # 얼굴인식 일치 정도가 60% 이상일때
-                if (int(confidence) > 50):
+                gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-                    # 출입성공시 출입로그 json 저장
-                    create_json(id)
+                faces = faceCascade.detectMultiScale(
+                    gray,
+                    scaleFactor = 1.2,
+                    minNeighbors = 5,
+                    minSize = (int(minW), int(minH)),
+                   )
 
-                    # 출입성공시 캡쳐, 얼굴인식상태 true
-                    face_recog_state=successCap(id)
+                for(x,y,w,h) in faces:
 
-                    face_name=id
-                    return 100
-                    print("얼굴인식이 되었습니다. 문이 열립니다.")
+                    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+                    id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+
+                    # Check if confidence is less them 100 ==> "0" is perfect match
+                    if (confidence < 100):
+                        id = names[id]
+                        confidence = "  {0}".format(round(100 - confidence))
+
+                        # 얼굴인식 일치 정도가 60% 이상일때
+                        if (int(confidence) > 50):
+
+                            # 출입성공시 출입로그 json 저장
+                            create_json(id)
+
+                            # 출입성공시 캡쳐, 얼굴인식상태 true
+                            face_recog_state=successCap(id)
+
+                            face_name=id
+                            GPIO.output(Sled, True)
+                            print("얼굴인식이 되었습니다. 문이 열립니다.")
+                            time.sleep(2)
 
 
-            else:
-                id = "unknown"
-                confidence = "  {0}".format(round(100 - confidence))
-                return 200
+                    else:
+                        id = "unknown"
+                        confidence = "  {0}".format(round(100 - confidence))
 
-            cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-            cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
+                        GPIO.output(Fled, False)
+                        print("얼굴인식에 실패했습니다")
+                    	time.sleep(2)
+                        cctv.Camera_cctv() #esc를 입력해야지 cctv동영상 촬영 종료
+                        break
+
+                    cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+                    cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
 
 
-        cv2.imshow('camera',img)
+                cv2.imshow('camera',img)
 
-        # if r == 20:
-        #     break
+                # if r == 20:
+                #     break
 
-        if face_recog_state == True:
-            break
+                if face_recog_state == True:
+                    break
 
-        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-        if k == 27:
-            break
+                k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+                if k == 27:
+                    break
 
 
 
@@ -127,3 +155,4 @@ class recognition():
     print("\n [INFO] Exiting Program and cleanup stuff")
     cam.release()
     cv2.destroyAllWindows()
+    GPIO.cleanup()
